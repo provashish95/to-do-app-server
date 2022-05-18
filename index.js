2a09d7630d10ed03aff7905f5629d84f53211b3a
Provashish Roy <provashishroy95@gmail.com>
2022-05-19 01:12:25 +0600

    conectiom to mongodb

--- index.js
+++ index.js
@@ -5,10 +5,27 @@ const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
-
+//to-do
+//JLQA8S4HibwczHaR
 //middleware 
 app.use(cors())
 app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m0zh9.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const tasksCollection = client.db("toDoList").collection("tasks");
+        console.log('db is connected');
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
